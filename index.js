d80e263d37a97b1fc80a9a971f9b444b2e6cfed2
zohaibkhanse <122855153+zohaibkhanse@users.noreply.github.com>
2023-12-21 08:58:45 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const openBtnEl = document.querySelector('.open-menu-btn');
+const closeBtnEl = document.querySelector('.close-menu-btn');
+const navEl = document.querySelector('.nav');
+const overlay = document.querySelector('.overlay');
+
+function hideMenu(){
+    navEl.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+function showMenu(){
+    navEl.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+openBtnEl.addEventListener('click', showMenu);
+closeBtnEl.addEventListener('click', hideMenu);
+overlay.addEventListener('click', hideMenu);
+navEl.addEventListener('click', function(e){
+    if(e.target.closest('a')){
+        hideMenu();
+    }
+});
+
+
